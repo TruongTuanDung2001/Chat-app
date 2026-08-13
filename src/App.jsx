25d2ecf07b1ae23c77a1cde7b3401edf0cd33a48
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-08-13 22:55:41 +0700

    Link to route page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+// import { Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import LoginPage from "./pages/Login/Login";
 
 function App() {
   return (
     <div>
       <Navbar />
       <h1 className="text-2xl font-bold text-blue-600">Hello World!</h1>
+
+      {/* pages */}
+      <Router />
     </div>
+  );
+}
+
+// function router dom
+function Router() {
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
 // end
 export default App;
+export { Router };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar(){
         <div className="navbar">
             <ul>
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link to="/"></Link>
                 </li>
                 <li>
                     <Link to="/login">Login</Link>
