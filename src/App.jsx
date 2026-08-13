f1c6c19436fcf8c9cb1f888e5690e1bf114bcb59
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-08-13 22:19:47 +0700

    Add content file navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div>
-      <h1 className="text-2xl font-bold text-blue-600">
-        Hello World!
-      </h1>
+      <Navbar />
+      <h1 className="text-2xl font-bold text-blue-600">Hello World!</h1>
     </div>
   );
 }
-
 
 // end
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+export default function Navbar(){
+    return (
+        <div className="navbar">
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+                <li>
+                    <Link to="/chat">Chat</Link>
+                </li>
+                <li>
+                    <Link to="/profile">Profile</Link>
+                </li>
+                <li>
+                    <Link to="/users">User profile</Link>
+                </li>
+                <li>
+                    <Link to="/admin">Admin</Link>
+                </li>
+                <li>
+                    <Link to="/admin/users">Manager users</Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
