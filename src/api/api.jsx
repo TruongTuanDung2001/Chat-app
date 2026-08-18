c1e27c08e347055b8053ec6e36bff84660cc54bb
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-08-18 14:35:50 +0700

    Handle function data get user by id

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,16 +1,30 @@
 const API_URL = "http://localhost:3001";
 
+// Get all users api
 async function getUsers() {
-    try {
-        const response = await fetch(`${API_URL}/users`);
+  try {
+    const response = await fetch(`${API_URL}/users`);
 
-        if(!response.ok) throw new Error("No get data users");
-        
-        const data = await response.json();        
-        return data;
-    } catch (error) {
-        throw error;
-    }
+    if (!response.ok) throw new Error("No get data users");
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
 }
 
+// Get user by id_user
+async function getUserById(id_user) {
+  try {
+    const response = await fetch(`${API_URL}/${id_user}}`);
+    if (!response.ok) throw new Error("Get user by id fails");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+//end
 export { getUsers };
